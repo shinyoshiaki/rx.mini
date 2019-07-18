7d0bcd6d194f2430f06fd23d21b4964b3e703e12
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-07-18 18:04:45 +0900

    v0.1.0

--- lib/core/index.d.ts
+++ lib/core/index.d.ts
@@ -1,13 +1,29 @@
-declare type EventFunc<T> = (data: T) => void;
+declare type EventExecute<T> = (data: T) => void;
+declare type EventComplete = () => void;
+declare type EventError = (e: any) => void;
 export default class Event<T = null> {
     private event;
     execute: (data: T) => void;
-    executeNull: () => void;
-    subscribe: (func: EventFunc<T>) => {
+    complete: () => void;
+    error: (e: any) => void;
+    allUnsubscribe: () => void;
+    subscribe: (execute: EventExecute<T>, complete?: EventComplete | undefined, error?: EventError | undefined) => {
         unSubscribe: () => void;
     };
-    allUnsubscribe: () => void;
-    once: (func: EventFunc<T>) => void;
+    once: (execute: EventExecute<T>, complete?: EventComplete | undefined, error?: EventError | undefined) => void;
     asPromise: (timelimit?: number | undefined) => Promise<T>;
+    readonly returnTrigger: {
+        execute: (data: T) => void;
+        error: (e: any) => void;
+        complete: () => void;
+    };
+    readonly returnListener: {
+        subscribe: (execute: EventExecute<T>, complete?: EventComplete | undefined, error?: EventError | undefined) => {
+            unSubscribe: () => void;
+        };
+        once: (execute: EventExecute<T>, complete?: EventComplete | undefined, error?: EventError | undefined) => void;
+        asPromise: (timelimit?: number | undefined) => Promise<T>;
+    };
+    readonly length: number;
 }
 export {};
--- lib/extensions/buffer/index.d.ts
+++ lib/extensions/buffer/index.d.ts
@@ -1,8 +1,8 @@
 import Event from "../../core";
 export declare function Buffer<T>(length: number, event: Event<T>): {
-    subscribe: (func: (data: T) => void) => {
+    subscribe: (execute: (data: T) => void, complete?: (() => void) | undefined, error?: ((e: any) => void) | undefined) => {
         unSubscribe: () => void;
     };
     asPromise: (timelimit?: number | undefined) => Promise<T>;
-    once: (func: (data: T) => void) => void;
+    once: (execute: (data: T) => void, complete?: (() => void) | undefined, error?: ((e: any) => void) | undefined) => void;
 };
